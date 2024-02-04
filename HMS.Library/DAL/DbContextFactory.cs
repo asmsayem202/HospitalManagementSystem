5efbaeb0c12adca17a3aff0043893b72710add26
IDB LAB-1 PC -6 <IDB LAB-1 PC -6@DESKTOP-TQDGPQA>
2024-02-04 15:43:56 +0600

    .

---
+++ HMS.Library/DAL/DbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace HMS.Library.DAL
+{
+	public class DbContextFactory : IDesignTimeDbContextFactory<HMSdb>
+	{
+		public HMSdb CreateDbContext(string[] args)
+		{
+			var optionBuilder = new DbContextOptionsBuilder();
+			optionBuilder.UseSqlServer("");
+			return new HMSdb(optionBuilder.Options);
+		}
+	}
+}
